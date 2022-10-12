9f07895076875ee0657244158032cd288fc0cff7
Đinh Ngọc Vân <57166656+DinhNgocVan@users.noreply.github.com>
2022-10-12 23:49:45 +0700

    Update BMI.java

--- src/BMI.java
+++ src/BMI.java
@@ -6,8 +6,8 @@ public class BMI {
     float weight;
 
     public BMI() {
-        height = 0;
-        weight = 0;
+        height = 0f;
+        weight = 0f;
     }
 
     public BMI(float weight, float height) {
@@ -39,22 +39,22 @@ public class BMI {
     public String BMIType() {
         String type = "";
         float bmi = BMI();
-        if(bmi < 18.5) {
+        if(bmi < 18.5f) {
             type = "gay";
         }
-        else if(bmi >= 18.5 && bmi <= 24.9) {
+        else if(bmi >= 18.5f && bmi <= 24.9f) {
             type = "binh thuong";
         }
-        else if(bmi >= 25 && bmi <= 29.9) {
+        else if(bmi >= 25f && bmi <= 29.9f) {
             type = "tang can";
         }
-        else if(bmi >= 30 && bmi <= 34.9) {
+        else if(bmi >= 30f && bmi <= 34.9f) {
             type = "beo phi cap do 1";
         }
-        else if(bmi >= 35 && bmi <= 39.9) {
+        else if(bmi >= 35f && bmi <= 39.9f) {
             type = "beo phi cap do 2";
         }
-        else if(bmi >= 40) {
+        else if(bmi >= 40f) {
             type = "beo phi cap do 3";
         }
         return type;
@@ -63,7 +63,7 @@ public class BMI {
 
 
     public String run() {
-        if (weight < 20 || weight > 300 || height < 1.3 || height > 2.8) {
+        if (weight < 20f || weight > 300f || height < 1.3f || height > 2.8f) {
             return ("khong hop le");
         } else {
             return ("BMI:" + BMI() + " " + "BMIType" + BMIType());
